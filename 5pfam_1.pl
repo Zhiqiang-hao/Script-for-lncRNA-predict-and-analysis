14f2d0081b5b86451df67234f8207b53024e1895
Zhiqiang-hao <70687768+Zhiqiang-hao@users.noreply.github.com>
2022-04-11 16:54:15 +0800

    Update 5pfam_1.pl

--- 5pfam_1.pl
+++ 5pfam_1.pl
@@ -1,15 +1,15 @@
 #!/usr/bin/perl
-open$in1,"/home/li/Myopsalax/LncRNA/v3/Lnc_identifiy/pip_out/4_non_nr"or die;
+open$in1,"$ARGV[0]"or die;#### Input file1: 4_non_nr
 while($in2=<$in1>){
 chomp$in2;
 $b{$in2}=1;
 }
-open$in1,"/home/li/Myopsalax/LncRNA/v3/CDS/Trinity.fasta.transdecoder_dir/longest_orfs.pep"or die;
+open$in1,"$ARGV[1]"or die; #### Input file2: longest_orfs.pep
 while($in2=<$in1>){
 if($in2=~/>(\w+):/){$a=1;
 $a=0 unless $b{$1};
 }
 $c.=$in2 if $a;
 }
-open$out,'>',"/home/li/Myopsalax/LncRNA/v3/Lnc_identifiy/pip_out/5_pep"or die;
+open$out,'>',"5_pep"or die; #### Output: 5_pep
 print$out($c);
