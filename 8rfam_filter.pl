614bebc9ac2705f10772f58c4230bc1040f2ecb4
Zhiqiang-hao <70687768+Zhiqiang-hao@users.noreply.github.com>
2022-04-11 17:33:06 +0800

    Update 8rfam_filter.pl

--- 8rfam_filter.pl
+++ 8rfam_filter.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
-open$in1,"/home/li/Other_studys/lncRNA_VVI/V5/pipline_out/rfam/rfam_class2"or die;
+open$in1,"$ARGV[0]"or die; #### Input file1: rfam_class2
 while($in2=<$in1>){
 if($in2=~/^(\S+)\s+(.*)/){
 $rfam{$1}=1;
@@ -8,7 +8,7 @@ $gen{$1}=$2;
 }
 }
 
-open$in1,"/home/li/Myopsalax/LncRNA/v3/CDS/rfam/rfam_out"or die;
+open$in1,"$ARGV[1]"or die; #### Input file2: rfam_out
 while($in2=<$in1>){
 if($in2=~/^(\S+)_i\d+\s+(\w+)/){
 $nam=$1;$rf=$2;
@@ -17,7 +17,7 @@ $hou{$nam}.="\t".$rf."\t".$gen{$rf}.";" if $rfam{$rf};
 }
 }
 
-open$in1,"/home/li/Myopsalax/LncRNA/v3/Lnc_identifiy/pip_out/7_non_cpat"or die;
+open$in1,"$ARGV[2]"or die; #### Input file3: 7_non_cpat
 while($in2=<$in1>){chomp$in2;
 if($in2=~/^(\S+)/){$nam=$1;
 if($hou{$nam}){
@@ -26,7 +26,7 @@ else{
 $d.=$nam."\n";
 }}}
 
-open$out,'>',"/home/li/Myopsalax/LncRNA/v3/Lnc_identifiy/pip_out/8_house_rfam2" or die;
+open$out,'>',"8_house_rfam" or die; #### Output file1: 8_house_rfam
 print$out($c);
-open$out,'>',"/home/li/Myopsalax/LncRNA/v3/Lnc_identifiy/pip_out/8_non_rfam2" or die;
+open$out,'>',"8_non_rfam2" or die; #### Output file2: 8_non_rfam2
 print$out($d);
