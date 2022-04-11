c45eb9a29a7115e745cd845ab69a22f0f4394c1f
Zhiqiang-hao <70687768+Zhiqiang-hao@users.noreply.github.com>
2022-04-11 17:04:52 +0800

    Update 5pfam_filter.pl

--- 5pfam_filter.pl
+++ 5pfam_filter.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
-open$in1,"/home/li/Myopsalax/LncRNA/v3/Lnc_identifiy/pip_out/5_pep_pfam_out"or die;
+open$in1,"$ARGV[0]"or die; #### Input file1: 5_pep_pfam_out
 while($in2=<$in1>){
 if($in2=~/^(TRINITY\S+?)::\S+\s+\S+\s+\S+\s+(\S+)\s+(\S+)/){
 $nam=$1;
@@ -10,7 +10,7 @@ $pfam{$nam}.=$2.";";print$nam."\t".$1."\n";
 }
 }
 
-open$in1,"/home/li/Myopsalax/LncRNA/v3/Lnc_identifiy/pip_out/4_non_nr"or die;
+open$in1,"$ARGV[1]"or die; #### Input file2: 4_non_nr
 while($in2=<$in1>){chomp$in2;
 if($in2=~/^(\S+)/){$nam=$1;#print$nam."\n";
 if($pfam{$nam}){print$nam;
@@ -19,7 +19,7 @@ else{
 $d.=$nam."\n";
 }}}
 
-open$out,'>',"/home/li/Myopsalax/LncRNA/v3/Lnc_identifiy/pip_out/5_pfam" or die;
+open$out,'>',"5_pfam" or die; #### Output file1: 5_pfam
 print$out($c);
-open$out,'>',"/home/li/Myopsalax/LncRNA/v3/Lnc_identifiy/pip_out/5_non_pfam" or die;
+open$out,'>',"5_non_pfam" or die; #### Output file2: 5_non_pfam
 print$out($d);
